caef5a6e492b304d8ed117807997525210d6b59c
ycharrouf <ycha0707@g.educaand.es>
2025-05-21 00:44:17 +0200

    server

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 import express from 'express'
 const app = express();
-
+import path from 'path'
+import { fileURLToPath } from 'url';
 import { UserController } from './Controllers/UserController.js';
 
 const PORT = process.env.PORT ?? 1234;
 
+const __filename = fileURLToPath(new URL(import.meta.url));
+const __dirname = path.dirname(__filename)
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/:name', UserController.getUser)
-app.get('/', UserController.getAll)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+app.post('/register', UserController.registerUser)
 
 
 app.listen(PORT, () => {

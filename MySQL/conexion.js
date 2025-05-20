a17bbafb989ea9c64a540806ec24d3230d5b7f25
ycharrouf <ycha0707@g.educaand.es>
2025-05-21 00:45:01 +0200

    conexion mysql

--- MySQL/conexion.js
+++ MySQL/conexion.js
@@ -1,2 +1,22 @@
-import mysql from 'mysql2'
+import mysql from 'mysql2/promise'
 import 'dotenv/config'
+
+const HOST = process.env.DB_HOST
+const USER = process.env.DB_USER
+const DATABASE = process.env.DB_DATABASE
+const PORT = process.env.DB_PORT
+const PASSWORD = process.env.DB_PASSWORD
+
+export class conexion {
+    static async createConexion() {
+        const conexion = await mysql.createConnection({
+            host: HOST,
+            database: DATABASE,
+            user: USER,
+            port: PORT,
+            password: PASSWORD
+        })
+
+        return conexion
+    }
+}

5c956cdcd3e942fb32ea7e481432ceb6fa91fae1
ycharrouf <ycha0707@g.educaand.es>
2025-05-21 00:44:52 +0200

    arquitectura mvc user empezado

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -7,11 +7,20 @@ export class UserController {
     }
 
     static async getUser(req, res) {
-        const {name} = req.params
+        const { name } = req.params
         const userFind = UserModel.FindUser(name)
-        if(!userFind) return res.status(400).send("No se ha encontrado ningun usuario")
-        
+        if (!userFind) return res.status(400).send("No se ha encontrado ningun usuario")
+
         res.json(userFind)
-        
+
+    }
+
+    static async registerUser(req, res) {
+        const user = req.body
+        const fields = UserModel.register({ user })
+
+        if (fields <= 0) return res.status(400).send('Error al registrar usuario')
+
+        res.send('usuario registrado correctamente')
     }
 }
--- Models/UserModel.js
+++ Models/UserModel.js
@@ -1,16 +1,33 @@
 import { users } from "../users.js";
+import { conexion } from '../MySQL/conexion.js'
 
 export class UserModel {
+
+
     static obtenerUsuarios() {
         return users
     }
 
-    static FindUser(name){
+    static FindUser(name) {
         const user = users.find(u => {
             return u.name.toLowerCase() == name.toLowerCase()
         });
         console.log(user);
-        
+
         return user;
+    }
+
+    static async register({ user }) {
+        console.log(user);
+        
+
+        let conex = await conexion.createConexion();
+        const date = new Date();
+        const fecha_alta = date.toISOString().split('T')[0];
+
+        const [row, fields] = await conex.execute(
+            'INSERT INTO Clientes (nombre,apellido,fecha_nacimiento,direccion,telefono,email,dni,fecha_alta) VALUES (?,?,?,?,?,?,?,?)',
+            [user.nombre, user.apellidos, user.fecha_nacimiento, user.direccion, user.telefono, user.email, user.dni, fecha_alta]
+        )
     }
 }

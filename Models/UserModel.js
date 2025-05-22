eac3c9ab2167dbdcd28808f7f25a6ee19b5f4731
ycharrouf <ycha0707@g.educaand.es>
2025-05-23 00:47:22 +0200

    refactor: enhance UserModel methods for better error handling and data validation

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -2,46 +2,126 @@ import { pool } from '../MySQL/conexion.js'
 
 export class UserModel {
 
-
-    static async getAllUsers() {
-        return users
-    }
-
-    static FindUser(name) {
-        const user = users.find(u => {
-            return u.name.toLowerCase() == name.toLowerCase()
-        });
-        console.log(user);
-
-        return user;
-    }
-
-    /* tema de la creación de conexion esta mal, ya que no se tiene que crear en cada funcion que se haga */
-
+    /**
+     * 
+     * @param {String} dni primary key of each user
+     * @returns the user object
+     */
     static async getUser(dni) {
-        const [row, fields] = await pool.execute(
-            'SELECT * FORM Clientes where DNI= ?',
-            [dni]
-        )
-        
+        if (!dni) {
+            throw new Error('DNI es requerido para obtener la información del usuario.');
+        }
+        try {
+            const [rows, fields] = await pool.execute(
+                'SELECT * FROM Clientes WHERE DNI = ?',
+                [dni]
+            );
 
+            if (rows.length > 0) {
+                return rows[0];
+            } else {
+                return null;
+            }
+
+        } catch (error) {
+            console.error('Error al obtener usuario por DNI');
+            throw new Error('Error de base de datos al intentar obtener el usuario.');
+        }
     }
 
+    /**
+     * 
+     * @param {object} object user for registration in database 
+     * @returns true or false depending of affected rows on database
+     */
     static async register({ user }) {
+        if (!user) return new Error('Es necesario los datos del usuario para poder registrarlo correctamente')
+
         const date = new Date();
-        const fecha_alta = date.toISOString().split('T')[0];/* create fecha_alta */
+        const fecha_alta = date.toISOString().split('T')[0];
 
         const [row, fields] = await pool.execute(
             'INSERT INTO Clientes (nombre,apellido,fecha_nacimiento,direccion,telefono,email,dni,fecha_alta) VALUES (?,?,?,?,?,?,?,?)',
             [user.nombre, user.apellidos, user.fecha_nacimiento, user.direccion, user.telefono, user.email, user.dni, fecha_alta]
-        )/* insert values into clientes */
+        )
+
+        if (row.affectedRows > 0) {
+            return true
+        } else {
+            return false
+        }
     }
 
-    static async update() {
+    /**
+     * 
+     * @param {object} user for update his info 
+     * @returns true or false depending of affected rows on database
+     */
+    static async update({ user }) {
+        if(!user) return new Error('Es necesario el usuario para la actualización')
 
+        /* Falta validar la información */
+        const userData = this.getUser(user.dni)
+
+        if (!userData) {
+            throw new Error(`Usuario con DNI ${user.dni} no encontrado.`);
+        }
+
+        const updateUser = {
+            nombre: user.nombre ?? userData.nombre,
+            apellido: user.apellidos ?? userData.apellido,
+            fecha_nacimiento: user.fecha_nacimiento ?? userData.fecha_nacimiento,
+            direccion: user.direccion ?? userData.direccion,
+            telefono: user.telefono ?? userData.telefono,
+            email: user.email ?? userData.email,
+            dni: user.dni ?? userData.dni,
+        }
+
+        const [row, fields] = await pool.execute(
+            'UPDATE Clientes SET nombre = ?, apellido = ?, fecha_nacimiento = ?, direccion = ?, telefono = ?, email = ? WHERE dni = ?',
+            [
+                updateUser.nombre,
+                updateUser.apellido,
+                updateUser.fecha_nacimiento,
+                updateUser.direccion,
+                updateUser.telefono,
+                updateUser.email,
+                user.dni,
+            ]
+        );
+
+        if (row.affectedRows > 1) {
+            console.log('Usuario actualizado');
+            true
+        } else {
+            return false
+        }
     }
 
-    static async remove() {
+    /**
+     * 
+     * @param {String} dni of user for remove he in the database 
+     * @returns true or false depending of affected rows on database
+     */
+    static async remove(dni) {
+        if (!dni) {
+            throw new Error('DNI is required to remove a user.');
+        }
 
+        try {
+            const [rows, fields] = await pool.execute(
+                'DELETE FROM usuarios WHERE dni = ?',
+                [dni]
+            );
+
+            if(rows.affectedRows > 0){
+                return true
+            }else{
+                return false
+            }
+
+        } catch (error) {
+            throw new Error('Database error during user removal.');
+        }
     }
 }

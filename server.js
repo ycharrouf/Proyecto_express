358abb61e300d025f70b200c3a238c804e456680
ycharrouf <ycha0707@g.educaand.es>
2025-06-04 17:12:46 +0200

    Refactor server.js: reorder imports, improve comments, and streamline middleware configuration

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
 import express from 'express'
-export const app = express();
 import path from 'path'
 import session from 'express-session';
-import UserRouter from './routes/user.js';
-import cuentaRouter from './routes/cuenta.js';
 import { fileURLToPath } from 'url';
-import ProductoRouter from './routes/Productos.js';
 import fs from 'fs'
 import https from 'https'
+
+//Importamos los routers
+import ProductoRouter from './routes/Productos.js';
+import UserRouter from './routes/user.js';
+import cuentaRouter from './routes/cuenta.js';
+
+export const app = express();
 const PORT = process.env.PORT ?? 443;
 
 const __filename = fileURLToPath(new URL(import.meta.url));
 const __dirname = path.dirname(__filename)
 
-//configuración de los formatos recibidos de los datos
+//Midelware para parsear los datos
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-//configuración de certificado
-const options = {
-    key: fs.readFileSync(path.join(__dirname, 'certs/key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, 'certs/cert.pem')),
-}
 
 //Configuración de session
 app.use(session({
@@ -44,36 +42,42 @@ app.use('/img', express.static(path.join(distPath, 'img')));
 app.use('/Fonts', express.static(path.join(distPath, 'Fonts')));
 app.use('/Assets', express.static(path.join(distPath, 'assets')));
 
-//Rutas de enrutadores
+//Rutas principales
 app.use('/User', UserRouter)
 app.use('/Cuenta', cuentaRouter)
 app.use('/Productos', ProductoRouter)
 
 //Rutas estaticas
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
     res.sendFile(path.join(distPath, 'index.html'))
 })
-
-app.get('/ayuda', (req, res) => {
+app.get('/ayuda', (_, res) => {
     res.sendFile(path.join(distPath, 'PreguntasFrecuentes', 'index.html'))
 })
-app.get('/info', (req, res) => {
+app.get('/info', (_, res) => {
     res.sendFile(path.join(distPath, 'Info', 'index.html'))
 })
-app.get('/terminosCondiciones', (req, res) => {
+app.get('/terminosCondiciones', (_, res) => {
     res.sendFile(path.join(distPath, 'TerminosCondiciones', 'index.html'))
 })
-app.get('/Ubicacion', (req, res) => {
+app.get('/Ubicacion', (_, res) => {
     res.sendFile(path.join(distPath, 'Ubicacion', 'index.html'))
 })
 
 
 /* Pagina de Error */
-app.use((req, res, next) => {
+app.use((_, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'dist', 'Error', 'index.html'))
 })
 
-/* Puerto donde escucha el servidor */
+//configuración de certificado
+const options = {
+    key: fs.readFileSync(path.join(__dirname, 'certs/key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'certs/cert.pem')),
+}
+
+
+//Inicio del servidor, y el puerto donde escucha
 https.createServer(options, app).listen(PORT, () => {
     console.log(`Servidor esta funcionando, en https://localhost:443`);
     
